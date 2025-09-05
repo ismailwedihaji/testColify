2bc56e848cc6cc96867d21fbc562486fc4a35196
Ismail Mohammed <ismailwedihaji@gmail.com>
2025-09-05 21:02:16 +0200

    Update backend message to test deplsoyment

--- app/api/hello/route.js
+++ app/api/hello/route.js
@@ -3,7 +3,7 @@ import { NextResponse } from 'next/server'
 export async function GET() {
   try {
     return NextResponse.json({
-      message: "Hello from Coolify backend NEW  haji fenji🚀",
+      message: "Hello from Coolify backend NEW  haji fenjiiiii🚀",
       timestamp: new Date().toISOString(),
       env: process.env.NODE_ENV || 'development',
       deployment: 'single-app'
